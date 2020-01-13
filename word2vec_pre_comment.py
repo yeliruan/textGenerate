49212af25930604dd9901706bbeb36227a7597d0
shengxiaoyu <13314409603@163.com>
2020-01-13 14:31:42 +0800

    稍微更新了一下针对评论和电影简介生成训练word2vec数据的代码，主要是格式

--- word2vec_pre_comment.py
+++ word2vec_pre_comment.py
@@ -9,8 +9,61 @@ Created on Wed Jan  8 21:23:19 2020
 import csv
 import jieba
 import re
+import os
 
+def main():
+    # windows
+    root_dir = r'A:\研三\textGenerate'
+    # linux
+    # root_dir = '/home/shengyu/yeli/textGenerate'
 
+    dataset_path = os.path.join(root_dir,'dataset')
+    filename = os.path.join(os.path.join(dataset_path,'moviedata'),'comments.csv')
+    outputfilepath = os.path.join(dataset_path,'comments')
+
+    file_userdict = os.path.join(root_dir,'mystopswords.txt')  # 此处文件名为用户自定义的文件名，内容为不想被分开的词
+    jieba.load_userdict(file_userdict)
+
+    #停用词典
+    stopwords_path = os.path.join(root_dir,'stopwords.txt')
+
+    #这种公用的变量可以放在循环的外面，只创建一次
+    # 创建一个停用词列表
+    stopwords = stopwordslist(stopwords_path)
+    d = {}
+
+    with open(filename, 'r',encoding='utf8') as f:
+        reader = csv.reader(f)
+        print(type(reader))
+        for row in reader:
+            if d.__contains__(row[2]):  # 如果包含这个建，在该建后面追加
+                d[row[2]] = d[row[2]] + '。' + row[3]
+            else:
+                d[row[2]] = row[3]
+
+    d_list = list(d.keys())
+
+    print(d)
+    lastID = ''
+    file_name = ''
+    for item in d_list:  # 遍历
+        if item != lastID:  # 新的电影就新建文件
+            file_name = os.path.join(outputfilepath,'c_'+ item + '.txt')   # 新建文件
+            lastID = item
+
+        sentences = sentence_splitter(d[item])
+        with open(file_name, 'a+', encoding='utf8') as f1:
+            for sentence in sentences:
+                line = re.sub('[a-zA-Z]', '', sentence)
+                seg_list = seg_depart(line)
+                # seg_list = jieba.cut(sentence,cut_all=False,HMM=True)#对每一句进行分词
+
+                out_list = ' '.join(seg_list.split())
+                out_list = out_list + '\n'
+                print(out_list)
+                if len(out_list) > 4:
+                    f1.writelines(out_list)
+                    f1.flush()
 
 def sentence_splitter(sentence):
     # 输入一个段落，分成句子，可使用split函数来实现
@@ -22,16 +75,15 @@ def sentence_splitter(sentence):
     return new_sents
 
         
-def stopwordslist():
-    stopwords = [line.strip() for line in open('stopwords.txt',encoding='UTF-8').readlines()]
+def stopwordslist(stopwords_path):
+    stopwords = [line.strip() for line in open(stopwords_path,encoding='UTF-8').readlines()]
     return stopwords
 
-def seg_depart(sentence):
+def seg_depart(sentence,stopwords):
     # 对文档中的每一行进行中文分词
     print("正在分词")
     sentence_depart = jieba.cut(sentence.strip())
-    # 创建一个停用词列表
-    stopwords = stopwordslist()
+
    
     # 输出结果为outstr
     outstr = ''
@@ -43,48 +95,11 @@ def seg_depart(sentence):
                 outstr += " "
     return outstr
 
-filename = "dataset/moviedata/comments.csv"
-outputfilepath = "dataset/comments/"
-file_userdict = 'stopwords.txt' #此处文件名为用户自定义的文件名，内容为不想被分开的词
-jieba.load_userdict(file_userdict)
-
-d = {}
-
-with open(filename, 'r') as f:
-    reader = csv.reader(f)
-    print(type(reader))
-    for row in reader:
-        if d.__contains__(row[2]):#如果包含这个建，在该建后面追加
-            d[row[2]] = d[row[2]]+'。'+row[3]
-        else:
-            d[row[2]] = row[3]
-            
-
-d_list = list(d.keys())  
-
-print(d)
-lastID = ''
-file_name = ''
-for item in  d_list:#遍历
-    if item != lastID:#新的电影就新建文件
-        file_name = outputfilepath + item + '.txt'#新建文件
-        lastID = item
-
-    sentences = sentence_splitter(d[item])
-    with open(file_name,'a+',encoding='utf8') as f1:
-        for sentence in sentences:
-            line =re.sub('[a-zA-Z]','',sentence)
-            seg_list = seg_depart(line)
-            # seg_list = jieba.cut(sentence,cut_all=False,HMM=True)#对每一句进行分词
-
-            out_list = ' '.join(seg_list.split())
-            out_list  = out_list + '\n'
-            print(out_list)
-            if len(out_list) > 4:
-                f1.writelines(out_list)
-                f1.flush()
 
 
+if __name__=='__main__':
+    main()
+    print('end')
 
            
             
--- word2vec_pre_movies.py
+++ word2vec_pre_movies.py
@@ -9,10 +9,54 @@ Created on Wed Jan  8 21:23:19 2020
 import csv
 import jieba
 import re
+import os
 
-filename = "/Users/mac/Library/Mobile Documents/com~apple~CloudDocs/movies2.csv"
-outputfilepath = "/Users/mac/Desktop/movie/"
 
+def main():
+    # windows
+    root_dir = r'A:\研三\textGenerate'
+    # linux
+    # root_dir = '/home/shengyu/yeli/textGenerate'
+
+    dataset_path = os.path.join(root_dir,'dataset')
+    filename = os.path.join(dataset_path,'movies.csv')
+    outputfilepath = os.path.join(dataset_path,'movies')
+
+    file_userdict = os.path.join(root_dir,'mystopswords.txt')  # 此处文件名为用户自定义的文件名，内容为不想被分开的词
+    jieba.load_userdict(file_userdict)
+
+    #停用词典
+    stopwords_path = os.path.join(root_dir,'stopwords.txt')
+
+    #这种公用的变量可以放在循环的外面，只创建一次
+    # 创建一个停用词列表
+    stopwords = stopwordslist(stopwords_path)
+
+    with open(filename, 'r',encoding='utf8') as f:
+        reader = csv.reader(f)
+        print(type(reader))
+        next(f)
+        lastID = ''
+        file_name = ''
+
+        for row in reader:
+            if row[0] != lastID:  # 新的电影就新建文件
+                file_name = os.path.join(outputfilepath,'m_'+row[0] + '.txt')   # 新建文件
+                lastID = row[0]
+            sentences = sentence_splitter(row[2])
+
+            #打开一个文件记得要调用fl.cloes()关闭，但是用with open这种格式就可以自动关闭，建议使用with open
+            with open(file_name,'a+',encoding='utf8') as f1:
+                for sentence in sentences:
+                    line = re.sub('[a-zA-Z]', '', sentence)
+                    seg_list = seg_depart(line,stopwords)
+                    # seg_list = jieba.cut(sentence,cut_all=False,HMM=True)#对每一句进行分词
+
+                    out_list = ' '.join(seg_list.split())
+                    out_list = out_list + '\n'
+                    print(out_list)
+                    if len(out_list) > 4:
+                        f1.writelines(out_list)
 def sentence_splitter(sentence):
     # 输入一个段落，分成句子，可使用split函数来实现
     sentences = re.split('(。|！|\!|\.|？|\?)',sentence)         # 保留分割符    
@@ -22,17 +66,15 @@ def sentence_splitter(sentence):
         new_sents.append(sent)
     return new_sents
 
-        
-def stopwordslist():
-    stopwords = [line.strip() for line in open('/Users/mac/Library/Mobile Documents/com~apple~CloudDocs/毕业论文代码/stopwords.txt',encoding='UTF-8').readlines()]
+def stopwordslist(stopwords_path):
+    stopwords = [line.strip() for line in open(stopwords_path,encoding='UTF-8').readlines()]
     return stopwords
 
-def seg_depart(sentence):
+def seg_depart(sentence,stopwords):
     # 对文档中的每一行进行中文分词
     print("正在分词")
     sentence_depart = jieba.cut(sentence.strip())
-    # 创建一个停用词列表
-    stopwords = stopwordslist()
+
    
     # 输出结果为outstr
     outstr = ''
@@ -44,32 +86,13 @@ def seg_depart(sentence):
                 outstr += " "
     return outstr
 
-file_userdict = '/Users/mac/Library/Mobile Documents/com~apple~CloudDocs/毕业论文代码/mystopswords.txt' #此处文件名为用户自定义的文件名，内容为不想被分开的词
-jieba.load_userdict(file_userdict)
 
-with open(filename, 'r') as f:
-    reader = csv.reader(f)
-    print(type(reader))
-    next(f)
-    lastID = ''
-    file_name = ''
-    for row in reader:
-        if row[0] != lastID:#新的电影就新建文件
-            file_name = outputfilepath + row[0] + '.txt'#新建文件
-            lastID = row[0]
-        sentences = sentence_splitter(row[2])
-        for sentence in sentences:
-            line =re.sub('[a-zA-Z]','',sentence)
-            seg_list = seg_depart(line)
-           # seg_list = jieba.cut(sentence,cut_all=False,HMM=True)#对每一句进行分词
-           
-            out_list = ' '.join(seg_list.split())
-            out_list  = out_list + '\n'
-            print(out_list)
-            if len(out_list) > 4:
-                 f1 = open(file_name,'a+')
-                 f1.writelines(out_list)
-                 
+
+
+
+if __name__=='__main__':
+    main()
+    print('end')
                  
                 
            
