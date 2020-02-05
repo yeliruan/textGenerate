1af02c8c212c010f863c76d67d54defb12cd377a
ruanyeli <yeli_ruan@126.com>
2020-02-05 12:31:06 +0800

    修改文件路径

--- data_preprocess/gettopic1.py
+++ data_preprocess/gettopic1.py
@@ -101,72 +101,12 @@ def handle(origin_path,new_file_path,topics_list_save_path,low_thredshod):
 if __name__=='__main__':
     root_path = '/home/shengyu/yeli/textGenerate/dataset'
     #root_path = r'A:\研三\textGenerate\dataset'
-    inputfile = os.path.join(root_path,'movie_storyline_comment_topic.csv')
-    inputfile_new = os.path.join(root_path,'movie_storyline_comment_topic_new_small.csv')
+    inputfile = os.path.join(root_path,'movie_storyline_comment_topic_new_small.csv')
+    inputfile_new = os.path.join(root_path,'movie_storyline_comment_topic_new_small_new.csv')
     save_path = os.path.join(root_path,'topic_new.txt')
     handle(inputfile,inputfile_new,save_path,2)
     print('end')
 
-def something():
-    inputfile = '/home/shengyu/yeli/movie_storyline_comment_topic.csv'
-    inputfile_new ='/home/shengyu/yeli/movie_storyline_comment_topic_new.csv'
-    save_path = '/home/shengyu/yeli/textGenerate/topic.txt'
-    dict_save_path = '/home/shengyu/yeli/textGenerate/topic_dict.pkl'
-    movie_storyline_comment_topic_new = '/home/shengyu/yeli/movie_storyline_comment_topic_new.csv'
-    
-    
-    topic_set = set()
-    topic_dict = {}
-    topic_dict_delete = {}
-    
-    headers = ['MOVIE_ID', 'COMMENT','RATING','SROTYLINE','TOPIC'] 
-    
-    with open(inputfile, 'r',encoding='utf8') as f: #topic 长度大于0的另存为
-        reader = csv.reader(f)
-        print(type(reader))
-        next(f)
-        for row in reader:
-            rows = row[4].split(' ')
-            if len(rows) > 0 :
-                rows = [{'MOVIE_ID':row[0], 'COMMENT':row[1], 'RATING':row[2],'SROTYLINE':row[3],'TOPIC':row[4]}]
-                topic_rows = row[4].split(' ')
-                for rowitem in rows:
-                    if rowitem in topic_dict:
-                        topic_dict[rowitem] = topic_dict[rowitem]+1
-                    else:
-                        topic_dict[rowitem] = 1
-                write_csv(inputfile_new,headers,rows)           
-                    
-
-    for key in topic_dict:
-        if topic_dict[key] > 1:
-            topic_set.add(key)
-        else:
-            topic_dict_delete[key] = key    
-                    
-    with open(inputfile_new, 'r',encoding='utf8') as f,open(movie_storyline_comment_topic_new, 'r',encoding='utf8') as f1:
-         f_csv = csv.DictWriter(f1, headers)
-         f_csv.writeheader() 
-         reader = csv.reader(f)
-         print(type(reader))
-         next(f)
-         for row in reader:
-             topic_rows = row[4].split(' ')
-             topic_new = ''
-             for rowitem in topic_rows: 
-                 if rowitem not in topic_dict_delete:
-                     topic_new = topic_new + rowitem 
-                     topic_new = topic_new + ' '
-             rows = [{'MOVIE_ID':row[0], 'COMMENT':row[1], 'RATING':row[2],'SROTYLINE':row[3],'TOPIC':topic_new}]
-             write_csv(movie_storyline_comment_topic_new,headers,rows)
-   
-    
-    with open(dict_save_path, 'wb') as fo:     # 将数据写入pkl文件
-        pickle.dump(topic_dict, fo)
-       
-          
-    with open(save_path, 'w') as fo:     # 将数据写入pkl文件
-        fo.write(topic_set)
 
 def write_csv(outfilename, headers, rows):
     with open(outfilename, 'a',encoding='utf-8-sig')as f:
