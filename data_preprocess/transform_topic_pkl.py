01de251f771f3f09a8a0c34f5fce29820ca55fc0
ruanyeli <yeli_ruan@126.com>
2020-02-05 18:56:45 +0800

    转化topic为指定格式

---
+++ data_preprocess/transform_topic_pkl.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 5 18:12:57 2020
+
+@author: ruanyeli
+"""
+import numpy as np
+import os
+import pickle
+
+if __name__=='__main__':
+
+    origin_path = '/home/shengyu/yeli/textGenerate/dataset/topic_new.txt'
+    save_path = '/home/shengyu/yeli/textGenerate/dataset/cteg/topic_list_100.pkl'
+
+    # origin_path = '/Users/mac/Desktop/topic_new.txt'
+    # save_path = '/Users/mac/Desktop/topic_list_100.pkl'
+
+    topic = []
+    with open(origin_path, "r") as f,open(save_path, "wb") as w:  # 打开文件
+        data = f.read()  # 读取文件
+        data = data.split(" ")
+
+        pickle.dump(data, w)
+
+    f = open(save_path, 'rb')
+    topic_dict = pickle.load(f)
+    print(topic_dict)
+
