9675a3d0034ff4ba6d3087ea84c25183ee8016e3
ruanyeli <yeli_ruan@126.com>
2020-02-07 16:35:31 +0800

    解决out of range 问题

--- data_preprocess/TF_IDF.py
+++ data_preprocess/TF_IDF.py
@@ -35,29 +35,30 @@ def write_csv(outfilename, headers, rows):  # 写入内容
         f_csv.writerows(rows)
         f.close()
 
+if __name__=='__main__':
+    # filename = "/home/shengyu/yeli/movie_storyline_comment.csv"
+    # outfilename = "/home/shengyu/yeli/textGenerate/dataset/movie_storyline_comment_topic_similarity.csv"
 
-filename = "/home/shengyu/yeli/movie_storyline_comment.csv"
-outfilename = "/home/shengyu/yeli/textGenerate/dataset/movie_storyline_comment_topic_similarity.csv"
+    filename = '/Users/mac/Desktop/movie_storyline_comment.csv'
+    outfilename = '/Users/mac/Desktop/movie_storyline_comment_topic_similarity.csv'
 
-headers = ['MOVIE_ID', 'COMMENT', 'RATING', 'STORYLINE', 'TOPIC']
-with open(outfilename, 'a', encoding='utf-8-sig')as f0:  # 写入header
-    f_csv = csv.DictWriter(f0, headers)
-    f_csv.writeheader()
+    headers = ['MOVIE_ID', 'COMMENT', 'RATING', 'STORYLINE', 'TOPIC']
+    with open(outfilename, 'a', encoding='utf-8-sig')as f0:  # 写入header
+        f_csv = csv.DictWriter(f0, headers)
+        f_csv.writeheader()
 
-with open(filename, 'r') as f:
-    reader = csv.reader(f)
-    print(type(reader))
-    next(f)
-    for row in reader:
-        if len(row[1]) > 1:
-            if len(row[3]) > 1:
-                try:
-                    if tfidf_similarity(row[1], row[3]) > 0.2:
-                        rows = [{'MOVIE_ID': row[0], 'COMMENT': row[1], 'RATING': row[2], 'STORYLINE': row[3],
-                                 'TOPIC': row[4]}]
-                        write_csv(outfilename, headers, rows)
-                except:
-                    print('这条评论有问题', 'ID: ', row[0], 'COMMENT: ', row[1])
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        print(type(reader))
+        next(f)
+        for row in reader:
+            if len(row[1]) > 1:
+                if len(row[3]) > 1:
+                        if tfidf_similarity(row[1], row[3]) > 0.2:
+                            rows = [{'MOVIE_ID': row[0], 'COMMENT': row[1], 'RATING': row[2], 'STORYLINE': row[3]}]
+                            write_csv(outfilename, headers, rows)
+                            print(rows)
+
 
 
 
