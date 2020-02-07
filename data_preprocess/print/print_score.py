1839e20c260fd8bf4e14b7558b2077da1363c809
ruanyeli <yeli_ruan@126.com>
2020-02-08 02:41:37 +0800

    debug

--- data_preprocess/print/print_score.py
+++ data_preprocess/print/print_score.py
@@ -22,47 +22,7 @@ from matplotlib.font_manager import FontProperties
 import seaborn as sns
 import os
 
-def handle_commentlength_print(origin_file, imgsave_path):
-    with open(origin_file, 'r', encoding="utf-8") as f:
-        reader = csv.reader(f)
-        print(type(reader))
-
-        # 统计评论长度
-        # 绘制直方图
-        # 选取max_length
-
-        comments_length_list = []
-        count = 0
-        maxlength = 0
-
-        for row in reader:
-            count += 1
-            comments_length = len(list(row[2]))
-            if comments_length > maxlength:
-                maxlength = comments_length
-
-            comments_length_list.append(comments_length)
-        group = []
-        print('count',count)
-        print(maxlength)
-
-        for i in range(5):
-            group.append(i)
-        # sns.set_palette("hls") #设置所有图的颜色，使用hls色彩空间
-        # sns.distplot(storyline_length,color="b",bins=30,kde=True)
-
-        plt.hist(comments_length_list, group, histtype='bar', rwidth=0.8)
-        plt.legend()
-
-        plt.xlabel('comments_length_group')
-        plt.ylabel('number')
-        # plt.title('测试例子——直方图')
-        plt.savefig(imgsave_path)
-        plt.show()
-
-
-
-def handle_commentlength_print(origin_file, imgsave_path):
+def handle_score_print(origin_file, imgsave_path):
     with open(origin_file, 'r', encoding="utf-8") as f:
         reader = csv.reader(f)
         print(type(reader))
@@ -100,4 +60,4 @@ if __name__ == '__main__':
     save_path = '/home/shengyu/yeli/textGenerate/dataset/score_length.jpg'
 
     origin_file = os.path.join(root_path, 'comments.csv')#原始comments
-    handle_commentlength_print(origin_file, save_path)
+    handle_score_print(origin_file, save_path)
