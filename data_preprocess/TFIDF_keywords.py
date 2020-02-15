7ea2967f85e20394e421cca6234efec3397f5092
shengxiaoyu <13314409603@163.com>
2020-02-15 20:38:03 +0800

    修改获取topic为TF-IDF提取关键词+考虑名词、形容词、动词;去掉非汉字；去掉多余空格；将评论长度选为（10，50]个词长

--- data_preprocess/TFIDF_keywords.py
+++ data_preprocess/TFIDF_keywords.py
@@ -10,92 +10,106 @@ import jieba.analyse
 import os
 import csv
 
+def is_uchar(uchar):
+    """判断一个unicode是否是汉字"""
+    if uchar >= u'\u4e00' and uchar<=u'\u9fa5':
+            return True
+    """判断一个unicode是否是数字"""
+    if uchar >= u'\u0030' and uchar<=u'\u0039':
+            return True
+    """判断一个unicode是否是英文字母"""
+    if (uchar >= u'\u0041' and uchar<=u'\u005a') or (uchar >= u'\u0061' and uchar<=u'\u007a'):
+            return True
+    if uchar in (' '):
+            return True
+    return False
 
 def get_keywords(text):
     """
     :param text: 评论文本
     :return: topic
     """
+    #保留汉字、数字、英文和空格
+    text = ''.join(list(filter(lambda x:is_uchar(x),list(text)))).strip()
+    if(len(text)==0):
+        return
+    #选择词长范围为10~25的评论
+    words = list(filter(lambda x: x!='',text.split(' ')))
+    comments_length = len(words)
+    if 10 < comments_length <= 50:
 
-    comments_length = len(list(text))
+        top_k = int(comments_length // 5)
 
-    if 10 < comments_length < 100:
-
-        top_k = int(comments_length // 10)
-        keywords = "  ".join(jieba.analyse.extract_tags(text, topK=top_k, withWeight=False, allowPOS=()))
+        #tf-idf 抽取关键词
+        # keywords = list(jieba.analyse.extract_tags(text, topK=top_k, withWeight=False, allowPOS=()))
         '''
               sentence 需要提取的字符串，必须是str类型，不能是list
               topK 提取前多少个关键字
               withWeight 是否返回每个关键词的权重
-              allowPOS是允许的提取的词性，默认为allowPOS=‘ns’, ‘n’, ‘vn’, ‘v’，提取地名、名词、动名词、动词
+              allowPOS是允许的提取的词性，默认为allowPOS空
         '''
-
-        # keywords = (jieba.analyse.extract_tags(sentence, topK=10, allowPOS=(['n', 'v','vn'])))
+        #tf-idf + 名词，动词，形容词 抽取关键词
+        keywords2 = list(jieba.analyse.extract_tags(text, topK=top_k, allowPOS=(['n', 'v','a'])))
+        #textrank + 名词，动词，形容词抽取关键词
+        # keywords3 = list(jieba.analyse.textrank(text, topK=top_k, allowPOS=(['n', 'v','a'])))
         # print('动词，名次',keywords)
 
-        text_list = text.split(' ')
-        keywords_list = keywords.split(' ')
-        keywords_obj = {}
-        topic = []
-
-        # print(text_list)
-
-        for keyword in keywords_list:  # 得到keywords 数组
-            index = text_list.index(keyword)
-            keywords_obj[keyword] = index
-
-        # 这里对排序的规则进行了定义，x指元组，x[1]是值，x[0]是键
-        for item in sorted(keywords_obj.items(), key=lambda x: x[1]):
-            topic.append(item[0])
-
-        topic_str = ''
-
-        for topic_item in topic:
-            if topic_item != '':
-                topic_str = topic_str + topic_item + ' '
-
-        return [comments_length, topic_str.strip()]
+        return [comments_length, sort_keywords(keywords2,text).strip()]
     else:
         return
 
+def sort_keywords(keywords,text):
+    text_list = text.split(' ')
+    keywords_list = keywords
+    keywords_obj = {}
+    topic = []
 
-def write_csv(savefile, headers, rows):  # 写入内容
-    with open(savefile, 'a', encoding='utf-8-sig')as f:
-        f_csv = csv.DictWriter(f, headers)
-        # 如果需要写入标题就可以
-        f_csv.writerows(rows)
+    # print(text_list)
 
+    for keyword in keywords_list:  # 得到keywords 数组
+        index = text_list.index(keyword)
+        keywords_obj[keyword] = index
+
+    # 这里对排序的规则进行了定义，x指元组，x[1]是值，x[0]是键
+    for item in sorted(keywords_obj.items(), key=lambda x: x[1]):
+        topic.append(item[0])
+
+    return ' '.join(topic)
 
 if __name__ == '__main__':
     root_path = '/home/shengyu/yeli/textGenerate/dataset'
     # root_path = '/Users/mac/workspace/textGenerate/dataset/'
-    file_path = os.path.join(root_path, 'movie_storyline_comment_topic_similarity.csv')
+    # root_path = r'A:\研三\textGenerate\dataset'
+    file_path = os.path.join(root_path, 'movie_storyline_comment_similarity.csv')
     save_path = os.path.join(root_path, 'movie_storyline_comment_similarity_topic.csv')
 
     save_path_problem = os.path.join(root_path, 'movie_storyline_comment_similarity_topic_problem.csv')
 
     headers = ['MOVIE_ID', 'COMMENT', 'RATING', 'STORYLINE', 'TOPIC']
 
-    with open(save_path, 'a', encoding='utf-8-sig') as w:  # 写入header
-        f_csv = csv.DictWriter(w, headers)
-        f_csv.writeheader()
 
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r',encoding='utf-8-sig') as f,open(save_path, 'w', newline='',encoding='utf-8-sig') as w,\
+            open(save_path_problem, 'w', newline='',encoding='utf-8-sig') as wp:
         reader = csv.reader(f)
         print(type(reader))
         next(f)
+
+        writer = csv.DictWriter(w, headers)
+        writer.writeheader()
+
+        writer_problem = csv.DictWriter(wp,['MOVIE_ID', 'COMMENT', 'RATING', 'STORYLINE'])
+        writer_problem.writeheader()
         count = 0
         for row in reader:
             try:
                 topic = get_keywords(row[1])
                 if topic:
                     count += 1
-                    rows = [
-                        {'MOVIE_ID': row[0], 'COMMENT': row[1], 'RATING': row[2], 'STORYLINE': row[3],
-                         'TOPIC': topic[1]}]
-                    write_csv(save_path, headers, rows)
+                    rows = {'MOVIE_ID': row[0], 'COMMENT': row[1],'RATING': row[2], 'STORYLINE': row[3],
+                         'TOPIC': topic[1]}
+                    writer.writerow(rows)
             except ValueError:
-                rows = [{'MOVIE_ID': row[0], 'COMMENT': row[1], 'RATING': row[2], 'STORYLINE': row[3]}]
-                write_csv(save_path_problem, ['MOVIE_ID', 'COMMENT', 'RATING', 'STORYLINE'], rows)
-
+                rows = {'MOVIE_ID': row[0], 'COMMENT': row[1], 'RATING': row[2], 'STORYLINE': row[3]}
+                writer_problem.writerow(rows)
+    print('总样例数:%d' % count)
     print('end')
